333d7ddb3ef128b80f7384b34e5122e384cb1e39
AbleLincoln <maneyandrew@gmail.com>
2024-03-19 15:27:57 -0700

    adds tiktok

--- src/app/_footer.tsx
+++ src/app/_footer.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons'
 
 import logo from 'public/steady-logo-white.png'
 
@@ -9,13 +9,23 @@ export default function Footer() {
   return (
     <footer className="col-span-2 flex items-center justify-between bg-steady-green px-12 py-12">
       <Image src={logo} alt="Steady logo" height={30} />
-      <Link href="https://www.instagram.com/steadyfordating/" target="_blank">
-        <FontAwesomeIcon
-          icon={faInstagram}
-          className="text-3xl"
-          color="white"
-        />
-      </Link>
+
+      <div className="flex items-center">
+        <Link
+          href="https://www.instagram.com/steadyfordating/"
+          target="_blank"
+          className="mr-6"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="text-3xl"
+            color="white"
+          />
+        </Link>
+        <Link href="https://www.tiktok.com/@steadyfordating" target="_blank">
+          <FontAwesomeIcon icon={faTiktok} className="text-2xl" color="white" />
+        </Link>
+      </div>
     </footer>
   )
 }
