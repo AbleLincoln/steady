76cf9dc2b8105d28dc0d42725559cd94e2c5d4f2
AbleLincoln <maneyandrew@gmail.com>
2024-07-12 12:10:03 -0700

    keywordssss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,9 +33,11 @@ export default async function Home() {
             <p className="text-xl">
               So many people. So many options. So many things that can go
               weirdly wrong. That&apos;s why there&apos;s{' '}
-              <span className="text-steady-pink">Steady</span>. Practical dating
-              advice from real human coaches to help you navigate the
-              unpredictable world of falling in love.
+              <span className="text-steady-pink">
+                Steady online date coaching
+              </span>
+              . Practical dating advice from real human coaches to help you
+              navigate the unpredictable world of falling in love.
             </p>
           </div>
 
