a595230c1ba8d71740a9d359551d29860a9c4cc6
AbleLincoln <maneyandrew@gmail.com>
2024-08-12 16:37:28 -0700

    simplify studio

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -2,14 +2,4 @@ import type { StructureResolver } from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list()
-    .title('Blog')
-    .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) =>
-          item.getId() &&
-          !['post', 'category', 'author'].includes(item.getId()!),
-      ),
-    ])
+  S.documentList().schemaType('post').title('Blog').filter(`_type == 'post'`)
